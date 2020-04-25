0c6248fb2b156278c7836bee6dcc9cdedb212ca1
MERN1 <user.name>
2020-04-25 12:53:54 -0700

    Heroku again

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ app.get("/", (req,res) => {
 	res.json({ "hello": "I am happy to deploy our application" })
 })
 
+// test again
 app.get("/api/user/auth", auth, (req,res) => {
 	res.status(200).json({
 		_id: req._id,
